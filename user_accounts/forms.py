d614534dfc6ad9165e76a74baad1b5189fea0ddc
Abdul Marzouq <abdulmarzouq@gmail.com>
2020-04-20 18:15:17 +0530

    Proper Placement of the Customer Model

--- user_accounts/forms.py
+++ user_accounts/forms.py
@@ -1,7 +1,7 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
-from virtdibs.models import Customer
+from .models import Customer
 
 class UserSignUpForm(UserCreationForm):
     email = forms.CharField(max_length=254, widget=forms.EmailInput())
--- virtdibs/models.py
+++ virtdibs/models.py
@@ -1,13 +1,3 @@
 from django.db import models
-from django.contrib.auth.models import User
-from django.db.models.signals import post_save
-from django.dispatch import receiver
 
 # Create your models here.
-class Customer(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE,default = "")
-    fullname = models.CharField(max_length=50,null='True',blank='True')
-    DOB = models.DateField(null='True',blank='True')
-    coins = models.FloatField(null='True',blank='True',default=0.0)
-    def __str__(self):
-        return self.user.username
